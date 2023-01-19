3bfda2c314d5af7c30f80beebbb76d007eb2a465
saurabh <saurabhfarkade83@gmail.com>
2023-01-19 13:44:43 +0530

    backend-added

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+mongoose.set("strictQuery", false);
+
 const connection = mongoose.connect(process.env.MONGO_URL);
 
 module.exports = { connection };
