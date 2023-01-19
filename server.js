03e72ff3df4fda035029c4a44137fef9581be78c
saurabh <saurabhfarkade83@gmail.com>
2023-01-19 13:32:59 +0530

    test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { connection } = require("./config/db");
 const cors = require('cors')
 const app = express();
+
+
 const PORT = 9090;
 app.use(express.json());
 app.use(cors());
